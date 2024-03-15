d7727936c27bb6aae205670ff61b6eba57337616
Yash Mistry <yash.mistry.g43@gmail.com>
2024-03-15 19:39:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,19 +6,12 @@ set -eu
 # Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
 export PYTHONUNBUFFERED=true
 
-# Install Python 3 virtual env
-VIRTUALENV=.data/venv
+# Install Python 3 if needed (uncomment the following lines if Python 3 is not already installed)
+# sudo apt-get update
+# sudo apt-get install -y python3 python3-venv
 
-if [ ! -d $VIRTUALENV ]; then
-  python3 -m venv $VIRTUALENV
-fi
+# Install the requirements globally
+pip install -r requirements.txt
 
-if [ ! -f $VIRTUALENV/bin/pip ]; then
-  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
-fi
-
-# Install the requirements
-$VIRTUALENV/bin/pip install -r requirements.txt
-
-# Run a glorious Python 3 server
-$VIRTUALENV/bin/python3 app.py
+# Run the Python server
+python3 app.py
